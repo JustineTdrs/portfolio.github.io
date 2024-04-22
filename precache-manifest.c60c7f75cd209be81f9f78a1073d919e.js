bd0edf4d57de80a6356c3b5bd6fa9e7548f6bf21
JustineTadros <71759215+JustineTadros@users.noreply.github.com>
2024-04-22 23:35:08 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.c60c7f75cd209be81f9f78a1073d919e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386360950e8084da9663ebb1c2b23057",
+    "url": "/portefolio/index.html"
+  },
+  {
+    "revision": "2d0a524dc6ed0dbd8b56",
+    "url": "/portefolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "48e55b086da80ce59157",
+    "url": "/portefolio/static/css/main.64388ba0.chunk.css"
+  },
+  {
+    "revision": "2d0a524dc6ed0dbd8b56",
+    "url": "/portefolio/static/js/2.64353e48.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portefolio/static/js/2.64353e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e55b086da80ce59157",
+    "url": "/portefolio/static/js/main.a86ddfa6.chunk.js"
+  },
+  {
+    "revision": "a3778ec4c6eb31ddde91",
+    "url": "/portefolio/static/js/runtime-main.b0ac9467.js"
+  }
+]);
